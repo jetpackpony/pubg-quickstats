68fb92830de8b4d0de3af67cb8bc22f248a96521
jetpackpony <jetpackpony@gmail.com>
2019-09-11 18:01:27 +0300

    Add styles to matches

--- src/components/User/Match.js
+++ src/components/User/Match.js
@@ -1,39 +1,90 @@
 import React from 'react';
+import styles from './Match.module.css';
+import moment from 'moment';
+
+const mapNames = {
+  "Desert_Main": "Miramar",
+  "DihorOtok_Main": "Vikendi",
+  "Erangel_Main": "Erangel",
+  "Baltic_Main": "Erangel",
+  "Range_Main": "Camp Jackal",
+  "Savage_Main": "Sanhok"
+};
+const getMapName = (id) => mapNames[id];
 
 const Match = ({ matchData }) => {
+  const place = matchData.playerData.attributes.stats.winPlace;
+  const st = [
+    styles.match,
+    (place <= 10 && place > 1)
+      ? styles.top10
+      : (place === 1)
+          ? styles.winner
+          : ""
+  ].join(" ");
   return (
-    <>
+    <div className={st}>
       <div>
-        {matchData.attributes.createdAt}
+        {getMapName(matchData.attributes.mapName)}
       </div>
       <div>
-        {matchData.attributes.mapName}
+        {moment(matchData.attributes.createdAt).fromNow()}
       </div>
       <div>
-        Place: {matchData.playerData.attributes.stats.winPlace}
+        {moment(matchData.attributes.createdAt).format("H:mm")}
+      </div>
+
+      <div className={styles.placeCell}>
+        <span className={styles.label}>Place</span>
+        <span className={styles.value}>
+          {place}
+        </span>
       </div>
       <div>
-        Kills: {matchData.playerData.attributes.stats.kills}
+        <span className={styles.label}>Kill Place</span>
+        <span className={styles.value}>
+          {matchData.playerData.attributes.stats.killPlace}
+        </span>
+      </div>
+
+      <div>
+        <span className={styles.label}>Kills</span>
+        <span className={styles.value}>
+          {matchData.playerData.attributes.stats.kills}
+        </span>
       </div>
       <div>
-        Headshot Kills: {matchData.playerData.attributes.stats.headshotKills}
+        <span className={styles.label}>Knocks</span>
+        <span className={styles.value}>
+          {matchData.playerData.attributes.stats.DBNOs}
+        </span>
       </div>
       <div>
-        Kill Place: {matchData.playerData.attributes.stats.killPlace}
+        <span className={styles.label}>Assists</span>
+        <span className={styles.value}>
+          {matchData.playerData.attributes.stats.assists}
+        </span>
+      </div>
+
+      <div>
+        <span className={styles.label}>Damage</span>
+        <span className={styles.value}>
+          {Math.round(matchData.playerData.attributes.stats.damageDealt)}
+        </span>
       </div>
       <div>
-        Knocks: {matchData.playerData.attributes.stats.DBNOs}
+        <span className={styles.label}>Headshot Kills</span>
+        <span className={styles.value}>
+          {matchData.playerData.attributes.stats.headshotKills}
+        </span>
       </div>
       <div>
-        Assists: {matchData.playerData.attributes.stats.assists}
+        <span className={styles.label}>Longest Kill</span>
+        <span className={styles.value}>
+          {Math.round(matchData.playerData.attributes.stats.longestKill)}
+        </span>
       </div>
-      <div>
-        Damage: {Math.round(matchData.playerData.attributes.stats.damageDealt)}
-      </div>
-      <div>
-        Longest Kill: {Math.round(matchData.playerData.attributes.stats.longestKill)} m
-      </div>
-    </>
+    </div>
   );
 };
 
--- src/components/User/Match.stories.js
+++ src/components/User/Match.stories.js
@@ -10,4 +10,13 @@ import testMatchesData from './getMatchData-plucked.json';
 storiesOf('Match', module)
   .add('successful load', () => (
     <Match matchData={testMatchesData[1].matchData} />
-  ));
+  ))
+  .add('top 10', () => (
+    <Match matchData={testMatchesData[2].matchData} />
+  ))
+  .add('chicken dinner', () => (
+    <Match matchData={testMatchesData[3].matchData} />
+  ))
+  .add('failed to load', () => (
+    <Match matchData={testMatchesData[4].matchData} />
+  ))
