c205dcd548ef50daffae0dc84dcc41f31fad85ad
jetpackpony <jetpackpony@gmail.com>
2019-09-13 14:42:18 +0300

    Roll back to the default service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,8 +21,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  //if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  if ('serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
@@ -33,9 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      // Using a custom service worker while it's impossible to inject workbox config
-      //const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
