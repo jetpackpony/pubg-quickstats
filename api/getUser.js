fcba093b1b6ca48c730af0ae7326b11dea1f44a3
jetpackpony <jetpackpony@gmail.com>
2019-09-14 16:49:27 +0300

    Fix the error message for can't find user

--- api/getUser.js
+++ api/getUser.js
@@ -1,3 +1,4 @@
+const APIError = require('./APIError');
 const users = {
   "jetpackpony": { id: "account.ceb7f506c0034b9e844a72def363ccdb" },
   "Romanchello": { id: "account.7a918ec2edc846cb9ecb00ecd95a5d89" }
@@ -6,10 +7,10 @@ const getUser = (userName) => (
   new Promise((resolve, reject) => {
     (users[userName])
       ? resolve(users[userName])
-      : reject({
+      : reject(new APIError({
         status: 404,
         title: `Can't find user with username '${userName}'`
-      })
+      }))
   })
 );
 module.exports = getUser;
